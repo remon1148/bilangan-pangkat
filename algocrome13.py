fe698aef2971cc598791ad5d7a08cd8343f1f283
remon1148 <remonlau49@gmail.com>
2026-01-16 00:34:08 +0700

    Add files via upload

---
+++ algocrome13.py
@@ -0,0 +1,5 @@
+#konversi mata uang
+usd = float(input("masukan jumblah uang dalam USD:"))
+kurs =15000 #ini untuk dolar ke rupiah
+idr = usd * kurs 
+print(f"jumblah uang dalam rupiah adalah {idr} IDR")
