5a5444de5a8fe8e71ff6edeaeb5b13d01d203f11
remon1148 <remonlau49@gmail.com>
2026-01-16 00:05:56 +0700

    Add files via upload

---
+++ algocrome8.py
@@ -0,0 +1,6 @@
+#menghitung pangkat 
+#dalam phyton pangkat pake simbol **
+basis = float(input("masukan basis:"))
+pangkat = float(input("masukan pangkat:"))
+hasil = basis ** pangkat
+print(f"hasil dari {basis} pangkat {pangkat} adalah {hasil}")
